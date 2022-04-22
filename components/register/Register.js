7d0944cff0b27849710bad892334f49be2e49b95
Derrick S <97965171+derricks-hexaware@users.noreply.github.com>
2022-04-22 11:38:13 +0530

    Update Register.js

--- components/register/Register.js
+++ components/register/Register.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Alert } from "react-native";
-import { Input, Button } from "react-native-elements";
+import { Text, View, StyleSheet, Alert, TextInput, Button } from "react-native";
 import Login from '../login/Login';
 import Style from "./Style";
 
@@ -30,18 +29,18 @@ export class Register extends Component {
   render() {
     return (
       <View style={Style.container}>
-        <Input
+        <TextInput
           placeholder="E-mail"
           value={this.state.username}
           onChangeText={(text) => this.setState({ username: text })}
         />
-        <Input
+        <TextInput
           placeholder="Password"
           secureTextEntry={true}
           value={this.state.password}
           onChangeText={(text) => this.setState({ password: text })}
         />
-        <Input
+        <TextInput
           placeholder="Confirm password"
           secureTextEntry={true}
           value={this.state.confirmPassword}
