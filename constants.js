27be7925b2f8f387b79d97a3d3d90c1da0d8fcbd
Derrick S <97965171+derricks-hexaware@users.noreply.github.com>
2022-04-26 09:47:55 +0530

    Delete constants.js

--- constants.js
+++
@@ -1,6 +0,0 @@
-export const tabBackgroundColor = "#faf7f7";
-export const tabIconColor = "#000000";
-export const searchBarBackgroundColor = "red";
-export const headerColor = "#0C120C";
-export const primaryColor = "#e63737";
-export const secondaryColor = "#FFFFFC";
