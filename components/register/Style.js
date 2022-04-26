8d925b591da3dc845bf9c8d9fae2997d01bde91b
Derrick S <97965171+derricks-hexaware@users.noreply.github.com>
2022-04-26 09:50:00 +0530

    Delete components directory

--- components/register/Style.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet, Alert } from "react-native";
-
-export default StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-    },
-  });
